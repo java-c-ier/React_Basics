4a294c8f9d102f8154b3dc81c4d6f13997fd2eb1
java-c-ier <jimutsahoo02@gmail.com>
2024-08-08 19:09:32 +0530

    Header updated

--- src/Routing/Components/UserDetail.jsx
+++ src/Routing/Components/UserDetail.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function UserDetail() {
-    const user = useParams()
-    
-    const navigate = useNavigate()
-    const GoBackHandler = () => navigate("/user")
-    // const GoBackHandler = () => navigate(-1)  Another way to go back
-    
+  const user = useParams();
+
+  const navigate = useNavigate();
+  const GoBackHandler = () => navigate("/user");
+  // const GoBackHandler = () => navigate(-1)  Another way to go back
+
   return (
-    <div className="w-1/2 m-auto mt-10 p-5">
-        <h1 className="text-3xl font-semibold text-red-400 mb-2">About</h1>
-        <p className="mt-2 mb-2 text-xl text-zinc-800">Hii {user.name}</p>
-        <button onClick={GoBackHandler} className="px-4 py-1 mt-1 text-white bg-red-400 transition duration-400 hover:bg-red-600 rounded-full">Back</button>
-      </div>
-  )
+    <div className="w-1/2 m-auto mt-1 p-5">
+      <h1 className="text-3xl font-semibold text-red-400 mb-2">User Detail</h1>
+      <p className="mt-2 mb-2 text-xl text-zinc-800">Hii {user.name}</p>
+      <button onClick={GoBackHandler} className="px-4 py-1 mt-1 text-white bg-red-400 transition duration-400 hover:bg-red-600 rounded-full">Back</button>
+    </div>
+  );
 }
 
-export default UserDetail
+export default UserDetail;
--- src/Routing/utils/Routing.jsx
+++ src/Routing/utils/Routing.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../Components/Home'
-import User from '../Components/User'
-import About from '../Components/About'
-import UserDetail from '../Components/UserDetail'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Components/Home";
+import User from "../Components/User";
+import About from "../Components/About";
+import UserDetail from "../Components/UserDetail";
 
 function Routing() {
   return (
     <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/user" element={<User />}></Route>
-        <Route path="/user/:name" element={<UserDetail />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-  )
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/user" element={<User />}>
+        <Route path="/user/:name" element={<UserDetail />}></Route>     {/* This make the UserDetail a child component of User */}
+      </Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  );
 }
 
-export default Routing
+export default Routing;
