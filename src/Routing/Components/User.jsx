2b375ece1ae22d7843825f25aa7a8a657cb6564a
java-c-ier <jimutsahoo02@gmail.com>
2024-08-08 18:45:22 +0530

    Updated user page.

--- src/Routing/Components/User.jsx
+++ src/Routing/Components/User.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function User() {
   return (
     <div>
       <div className="w-1/2 m-auto mt-10 p-5">
         <h1 className="text-3xl font-semibold text-red-400 mb-2">User</h1>
-        <p className="mb-3 text-zinc-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, blanditiis, quaerat iure architecto ab ducimus eaque dicta minima totam ipsa iusto voluptates!</p>
-        <button className="px-3 py-1 text-white bg-blue-500 rounded-full">Explore</button>
+        <div className="flex flex-col mt-5 items-left w-fit text-center">
+          <Link className="p-3 bg-red-300 text-xl m-2 transition duration-400 hover:bg-red-500 rounded-lg" to='/user/john'>John</Link>
+          <Link className="p-3 bg-red-300 text-xl m-2 transition duration-400 hover:bg-red-500 rounded-lg" to='/user/jack'>Jack</Link>
+          <Link className="p-3 bg-red-300 text-xl m-2 transition duration-400 hover:bg-red-500 rounded-lg" to='/user/jimmy'>Jimmy</Link>
+        </div>
       </div>
     </div>
   );
